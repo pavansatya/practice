26667ae9aea86967c72adcb276e2425df57f54d5
pavansatya <krishvenigalla06@gmail.com>
2025-02-19 19:01:15 -0500

    update

--- modules/additives.py
+++ modules/additives.py
@@ -1,8 +1,22 @@
 # Function to clean and split the additives_en column
 import pandas as pd 
+import re
 
-def clean_additives(additives):
-    if pd.isna(additives):
-        return 'No additives'
-    # Split by commas and remove leading/trailing whitespace
-    return list(set([additive.strip() for additive in str(additives).split(',')]))
+# def clean_additives(additives):
+#     if pd.isna(additives):
+#         return 'No additives'
+#     # Split by commas and remove leading/trailing whitespace
+#     return list(set([additive.strip() for additive in str(additives).split(',')]))
+
+
+
+
+# Function to extract additive codes
+def extract_codes(text):
+    # If the text is "No additives", return it as is
+    if text == 'No additives':
+        return text
+    # Use regex to find all additive codes (e.g., E102, E129)
+    codes = re.findall(r'E\d+\w*', text)
+    # Join the codes with commas
+    return ','.join(codes)
